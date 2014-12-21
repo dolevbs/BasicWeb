1f74959bce60f91e5f7e951cd78ae0168750a1ad
AviranJ <aviranj@gmail.com>
2014-12-21 12:18:07 +0200

    Update ServletCatagory.java

--- Trivia/src/ServletCatagory.java
+++ Trivia/src/ServletCatagory.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletCatagory extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String itemID = request.getParameter("itemID");
+        String CatagoryID = request.getParameter("itemID");
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -36,15 +36,22 @@ public class ServletCatagory extends HttpServlet {
             out.println("<body>");       
             out.println("<h1 align=\"center\" id=\"content\" style=\"font-weight:bold; color:#C8AC60;\">");
             out.println("Please select the category:</h1><br><br><br>");
-            out.println("<h2 align=\"center\" id=\"content\">");
+            out.println("<h3 align=\"center\" id=\"content\">");
             out.println("<form action=\"\">");
-            out.println("<input type=\"checkbox\" name=\"Catagory\" value=\"General\">General<br>");
-            out.println("<input type=\"checkbox\" name=\"Catagory\" value=\"Geography\">Geography<br>");
-            out.println("<input type=\"checkbox\" name=\"Catagory\" value=\"History\">History<br>");
-            out.println("<input type=\"checkbox\" name=\"Catagory\" value=\"Sports\">Sports<br>");
-            out.println("<INPUT TYPE=\"SUBMIT\" VALUE=\"Play\">\n");
+            out.println("<input type=\"checkbox\" name=\"Catagory1\" value=\"General\">General<br>");
+            out.println("<input type=\"checkbox\" name=\"Catagory2\" value=\"Geography\">Geography<br>");
+            out.println("<input type=\"checkbox\" name=\"Catagory3\" value=\"History\">History<br>");
+            out.println("<input type=\"checkbox\" name=\"Catagory4\" value=\"Sports\">Sports<br><br>");
+            out.println("</h3>"); 
+            out.println("<h2 align=\"center\" id=\"content\" style=\"font-weight:bold; color:#C8AC60;\">");
+            out.println("Please select the difficulty:</h2><br><br><br>");
+            out.println("<h3 align=\"center\" id=\"content\">");
+            out.println("<input type=\"radio\" name=\"difficulty\" value=\"Easy\">Easy<br>");
+            out.println("<input type=\"radio\" name=\"difficulty\" value=\"Medium\">Medium<br>");
+            out.println("<input type=\"radio\" name=\"difficulty\" value=\"Hard\">Hard<br>");
+            out.println("<br><br><br><INPUT TYPE=\"SUBMIT\" VALUE=\"Play\">\n");
             out.println("</form>");
-            out.println("</h2>");       
+            out.println("</h3>");       
             out.println("</body>");
             out.println("</html>");
         }
@@ -59,3 +66,4 @@ public class ServletCatagory extends HttpServlet {
     }
 
 }
+
